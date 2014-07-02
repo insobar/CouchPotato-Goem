045885db7dfb527c5d74e34b04c20356807c141f
Casey Milburn <casey.milburn@gmail.com>
2014-07-02 12:31:49 +1000

    Update goem.py

--- couchpotato/core/media/movie/providers/torrent/goem.py
+++ couchpotato/core/media/movie/providers/torrent/goem.py
@@ -1,7 +1,9 @@
 from couchpotato.core.media._base.providers.torrent.goem import Base
+from couchpotato.core.logger import CPLog
+from couchpotato.core.media.movie.providers.base import MovieProvider
 
 log = CPLog(__name__)
 
 autoload = 'Goem'
 
-class Goem(MovieProvider, Base):
+class Goem(MovieProvider, Base): pass

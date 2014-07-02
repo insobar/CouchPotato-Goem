271a6c31425e4b061bfcdc4d52ebe05ad43f8ee7
Casey Milburn <casey.milburn@gmail.com>
2014-07-02 19:22:47 +1000

    Update goem.py

--- couchpotato/core/media/_base/providers/torrent/goem.py
+++ couchpotato/core/media/_base/providers/torrent/goem.py
@@ -21,8 +21,6 @@ class Base(TorrentProvider):
         'search': 'http://goem.org/browse.php?search=%s&stype=2&cat=0',
     }
 
-    source = '&source%%5B%%5D=%s'
-
     def _find_quality_params(self, quality_id):
         quality_id = quality_id.upper()
         if quality_id == 'HD':
@@ -42,75 +40,46 @@ class Base(TorrentProvider):
         else:
             return None
 
-    def _get_page_count(self, html):
-        nav_div = html.find('p', attrs={'class', 'pager'})
-        if nav_div:
-            page_links = nav_div.find_all('a')
-            if page_links:
-                pages = [0]
-                for link in page_links:
-                    matcher = re.search('page=(\d*)', link['href'])
-                    pages.append(tryInt(matcher.group(0)))
-                return max(pages)
-        return 1
-
     def _add_torrent(self, table_row, results):
+	log.error('Goem Found %s', table_row)
         torrent_tds = table_row.find_all('td')
-        if len(torrent_tds) == 11:
-            url_base = self.urls['base_url']
-            torrent_id = torrent_tds[0].find('a')['href'].replace('/details.php?id=', '')
-            download_url = url_base + torrent_tds[1].find('a')['href']
-            details_link = torrent_tds[5].find('a')
-            details_url = url_base + details_link['href']
-            torrent_name = details_link.string
-            torrent_seeders = tryInt(re.sub('[^0-9]', '', unicode(torrent_tds[7].find('nobr').contents[0].string)))
-            torrent_leechers = tryInt(re.sub('[^0-9]', '', unicode(torrent_tds[7].find('nobr').contents[2].string)))
-            torrent_size = self.parseSize(torrent_tds[9].find('span').string)
-
-            results.append({
-                'id': torrent_id,
-                'name': torrent_name,
-                'url': download_url,
-                'detail_url': details_url,
-                'size': torrent_size,
-                'seeders': torrent_seeders,
-                'leechers': torrent_leechers,
-            })
+	url_base = self.urls['base_url']
+	torrent_id = torrent_tds[0].find('a')['href'].replace('/details.php?id=', '')
+	download_url = url_base + torrent_tds[1].find('a')['href']
+	details_link = torrent_tds[3].find('a')
+	details_url = url_base + details_link['href']
+	torrent_name = details_link.string
+	torrent_seeders = tryInt(re.sub('[^0-9]', '', unicode(torrent_tds[5].find('nobr').contents[0].string)))
+	torrent_leechers = tryInt(re.sub('[^0-9]', '', unicode(torrent_tds[5].find('nobr').contents[2].string)))
+	torrent_size = self.parseSize(torrent_tds[7].find('span').string)
+	results.append({
+		'id': torrent_id,
+		'name': torrent_name,
+		'url': download_url,
+		'detail_url': details_url,
+		'size': torrent_size,
+		'seeders': torrent_seeders,
+		'leechers': torrent_leechers,
+	})
     # noinspection PyBroadException
     def _search(self, media, quality, results):
-        quality_map = self._find_quality_params(quality['identifier'])
-        if quality_map is None:
-            return
-
-        quality = quality_map['tag']
-        use_source_tag = quality_map['param']
-
         imdb_id = getIdentifier(media)
-
-        current_page = 1
-        pages = -1
-
-        while True:
-            try:
-                url = self.urls['search'] % imdb_id
-                data = self.getHTMLData(url)
-
-                if data:
-                    html = BeautifulSoup(data)
-                    if pages == -1:
-                        pages = self._get_page_count(html)
-
-                    torrent_table = html.find('table', attrs={'id': 'browse'})
-                    if torrent_table:
-                        torrent_rows = torrent_table.find_all('tr', attrs={'class': 'table_row'})
-                        for row in torrent_rows:
-                            self._add_torrent(row, results)
-
-                    if current_page >= pages:
-                        break
-            except:
-                log.error('Unexpected error while searching %s: %s', (self.getName(), traceback.format_exc()))
-                return
+	try:
+		url = self.urls['search'] % imdb_id
+		data = self.getHTMLData(url)
+		if data:
+			html = BeautifulSoup(data)
+			torrent_table = html.find('table', attrs={'id': 'browse'})
+			if torrent_table:
+				torrent_rows = torrent_table.find_all('tr', attrs={'class': 'table_row'})
+				for row in torrent_rows:
+					self._add_torrent(row, results)
+				torrent_rows = torrent_table.find_all('tr', attrs={'class': 'table_row_alt'})
+				for row in torrent_rows:
+					self._add_torrent(row, results)
+	except:
+		log.error('Unexpected error while searching %s: %s', (self.getName(), traceback.format_exc()))
+		return
 
     def getLoginParams(self):
         return tryUrlencode({
@@ -121,7 +90,7 @@ class Base(TorrentProvider):
 
     def loginSuccess(self, output):
         return 'Login failed!' not in output.lower()
-		
+
 config = [{
     'name': 'goem',
     'groups': [
